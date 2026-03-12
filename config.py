590e44573a1e5999bdd88d85d5f30eebfb865bb7
Ashkelon <danilashakirov33@gmail.com>
2026-03-12 18:05:46 +0300

    fix

--- config.py
+++ config.py
@@ -7,7 +7,10 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
 # Строка подключения к базе (Render подставит целиком)
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 
 # Если есть Google Sheets
 GOOGLE_CREDS = os.getenv("GOOGLE_CREDS")  # если используешь
